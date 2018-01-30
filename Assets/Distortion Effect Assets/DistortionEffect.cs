cae2a78842bbe2f73589161a93415c8feadaa8b4
Marcos Morais <marcostmorais@gmail.com>
2018-01-29 23:40:31 -0200

    Refactored the example Distortion Effect Script

--- Assets/Distortion Effect Assets/DistortionEffect.cs
+++ Assets/Distortion Effect Assets/DistortionEffect.cs
@@ -4,70 +4,22 @@ using UnityEngine;
 
 public class DistortionEffect : MonoBehaviour {
 
-    [SerializeField] float m_fadeDuration = 0.01f;
-    [SerializeField] float m_scalingFactor = 0.01f;
-    [SerializeField] float m_alphaStart = 1.0f;
-    [SerializeField] float m_alphaEnd = 0.0f;
-    public bool m_useMaterialAlpha = false;
-
-    private float alphaDiff;
-    private float startTime;
-    private bool isFading = false;
-    private Renderer rend;
-    private Color fadeColor;
+    [SerializeField] float m_scalingFactor = 0.001f;
+    [SerializeField] float m_maxLocalScale = 0.01f;
 
 	// Use this for initialization
 	void Start () {
-        rend = GetComponent<Renderer>();
-
-        fadeColor = rend.material.color;
-
-        if (!m_useMaterialAlpha)
-        {
-            fadeColor.a = m_alphaStart;
-        }
-        else
-        {
-            m_alphaStart = fadeColor.a;
-        }
-
-        rend.material.color = fadeColor;
-        alphaDiff = m_alphaStart - m_alphaEnd;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        transform.localScale += new Vector3(m_scalingFactor, m_scalingFactor, 0.0f);
 
-        transform.localScale += new Vector3(m_scalingFactor, m_scalingFactor, transform.localScale.z);
-        fadeAlpha();
-
-	}
-
-    private void fadeAlphaUsing(float alphaFactor) {
-        
-    }
-
-    private void fadeAlpha() {
-        if (isFading)
-        {
-            var elapsedTime = Time.time - startTime;
-
-            if (elapsedTime <= m_fadeDuration)
-            {
-                var fadeProgress = elapsedTime / m_fadeDuration;
-                var alphaChange = fadeProgress * alphaDiff;
-                fadeColor.a = m_alphaStart - alphaChange;
-                rend.material.color = fadeColor;
-            }
-            else
-            {
-                fadeColor.a = m_alphaEnd;
-                rend.material.color = fadeColor;
-                isFading = false;
-                Destroy(gameObject);
-            }
+        if (transform.localScale.x > m_maxLocalScale) {
+            transform.localScale = new Vector3(
+                m_scalingFactor, m_scalingFactor, transform.localScale.z);
         }
-    }
+	}
 
 }
